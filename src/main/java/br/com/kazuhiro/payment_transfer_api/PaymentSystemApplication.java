3ba98fa04ad90dd09c7089c851253b871daee57e
kazuhirobruno <brunokazuhiro@gmail.com>
2026-06-23 19:47:51 -0300

    refactor: rename application to matches the github name

--- src/main/java/br/com/kazuhiro/payment_transfer_api/PaymentSystemApplication.java
+++ src/main/java/br/com/kazuhiro/payment_transfer_api/PaymentSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentTransferApiApplication {
+public class PaymentSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentTransferApiApplication.class, args);
+		SpringApplication.run(PaymentSystemApplication.class, args);
 	}
 
 }
